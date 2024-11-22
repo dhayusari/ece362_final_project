c366d05a9ad819c997af32aaa91223df5a06c16f
Tkikuno <tylerkikuno@gmail.com>
2024-11-22 12:35:10 -0500

    update sensor.c

--- src/sensor.c
+++ src/sensor.c
@@ -68,8 +68,7 @@ void update_display() {
         // spi2_display1("No Motion!");
 
         LCD_Clear(0xFFFF);
-        LCD_DrawString(85, 215, 0xFFFF, 0x0000, "NO", 16, 1);
-        LCD_DrawString(85, 215, 0xFFFF, 0x0000, "MOTION", 16, 1);
+        LCD_DrawString(85, 215, 0xFFFF, 0x0000, "NO MOTION", 16, 1);
     }
     if (state[0] != state[1]) {
         if (state[1] == 1) {
@@ -85,11 +84,7 @@ void update_display() {
                 nano_wait(2000000000);
                 //countdown();
                 alarm();
-            }
-
-            LCD_Clear(0xFFFF);
-            LCD_DrawString(85, 215, 0xFFFF, 0x0000, "MOTION", 16, 1);
-            LCD_DrawString(85, 215, 0xFFFF, 0x0000, "DETECTED", 16, 1);        
+            }     
         }
     }
 }
